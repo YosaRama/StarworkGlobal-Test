8af619c51cbf14abe37cfd93615468ecb7fd6b47
YosaRama <yosamelody07@gmail.com>
2020-09-30 11:40:46 +0800

    Add href route

--- src/view/NavItem.js
+++ src/view/NavItem.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function AddNavItem(props) {
   return (
     <li className="nav-item">
-      <a className="nav-link" href="lo">
+      <a className="nav-link" href={"#" + props.title + "Section"}>
         {props.title}
       </a>
     </li>
